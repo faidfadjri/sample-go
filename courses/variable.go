3961b487cae93828ae5202c4dc224ef1600738f9
faidfadjri <faidfadjri.busdev@gmail.com>
2023-10-10 00:12:28 +0700

    save

---
+++ courses/variable.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Strings
+	var firstName string = "Mohamad"
+	var lastName string = "Fadjri"
+	var middleName string
+
+	middleName = "Faid"
+	middleName = "Changes middleName"
+
+	fmt.Println(firstName + middleName + lastName)
+
+	// Simple way for initialize variable instead of the var otherName string = "Mohamad Faid Fadjri"
+	// Only the first time
+	otherName := "Mohamad Faid Fadjri"
+	fmt.Println(otherName)
+
+	// Int
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// Bits & memory
+	var numOne int8 = 120
+	fmt.Print(numOne)
+}
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// String
+	var Name string = "Faid Fadjri"
+	fmt.Println(Name)
+
+	// Automatically iniatied variable as a string``
+	changesName := "Hello World"
+	fmt.Println(changesName)
+}
