31217061bdd5fe9979887e1a549257cde2cd6ffc
faidfadjri <faidfadjri.busdev@gmail.com>
2023-10-13 00:29:39 +0700

    variable

--- courses/variable.go
+++ courses/variable.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+
+
+/*
+
+----------------------------------
+var int8Var int8 = -128 // Ini adalah deklarasi tipe data int8 dengan nilai minimal -128 hingga maksimal 127.
+var int16Var int16 = -32768 // Ini adalah deklarasi tipe data int16 dengan nilai minimal -32768 hingga maksimal 32767.
+var int32Var int32 = -2147483648 // Ini adalah deklarasi tipe data int32 dengan nilai minimal -2147483648 hingga maksimal 2147483647.
+var int64Var int64 = -9223372036854775808 // Ini adalah deklarasi tipe data int64 dengan nilai minimal -9223372036854775808 hingga maksimal 9223372036854775807.
+
+
+var uintVar uint = 127 // Ini adalah deklarasi tipe data uint dengan nilai minimal 0 hingga maksimal 255 (tergantung pada arsitektur platform).
+var float32Var float32 = 3.14 // Ini adalah deklarasi tipe data float32.
+var float64Var float64 = 3.141592653589793 // Ini adalah deklarasi tipe data float64.
+*/
+
 func main() {
 
 	// String
