eeb08bda4e9d1b4fb51e1d239ed551cd554e54b6
faidfadjri <faidfadjri.busdev@gmail.com>
2023-10-14 10:53:00 +0700

    closure

---
+++ courses/Function/closure.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// Closure (Lingkup kerja sebuah function atau variable)
+// Kemampuan sebuah function berinteraksi dengan data data di sekitarnya dalam scope yang sama
+
+
+func main(){
+	counter := 0
+	increment := func() {
+		fmt.Println("Increment")
+		counter++
+	}
+
+	increment()
+	increment()
+
+	fmt.Println(counter)
+}
--- courses/Function/function-recursive.go
+++ courses/Function/function-recursive.go
@@ -6,7 +6,7 @@ import "fmt"
 // Function yang memanggil dirinya sendiri
 // Factorial 5! = 5 * 3 * 2 * 1
 
-func main(){
+func RecursiveFunction(){
 	fmt.Println(Factorial(4))
 }
 
